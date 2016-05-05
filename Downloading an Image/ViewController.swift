789fcb1294771a2b716cb50249f02acee2d13e54
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:45:38 -0700

    Commit again

--- Downloading an Image/ViewController.swift
+++ Downloading an Image/ViewController.swift
@@ -10,16 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        var documentsDirectory: String?
+        
+        var paths: [AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        
+        if paths.count > 0 {
+            
+            documentsDirectory = paths[0] as? String
+            
+            let savePath = documentsDirectory! + "/grumpyCat.jpg"
+            
+            self.imageView.image = UIImage(named: savePath)
+            
+        }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+      
+       /* let imageUrl = NSURL(string: "https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg")
+        
+        // let request = NSURLRequest(imageUrl)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(imageUrl!) { (data, response, error) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                var documentsDirectory: String?
+                
+                var paths: [AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                
+                if paths.count > 0 {
+                    
+                    documentsDirectory = paths[0] as? String
+                    
+                    let savePath = documentsDirectory! + "/grumpyCat.jpg"
+                    
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                    
+                        self.imageView.image = UIImage(named: savePath)
+                        
+                    })
+                    
+                }
+                
+                /* dispatch_async(dispatch_get_main_queue(), {
+                    
+                    if let grumpyCat = UIImage(data: data!) {
+                        
+                        self.imageView.image = grumpyCat
+                        
+                    }
+                    
+                }) */
+                
+               
+            }
+            
+        }
+        
+        task.resume() */
+        
     }
-
 
 }
 
